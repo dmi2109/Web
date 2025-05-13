ec6d697589112c6137ee15b25a8a5c23512c9ad2
dmi2109 <maidieumi@gmail.com>
2025-05-13 13:19:28 +0700

    Buoi 3

---
+++ Buoi_3/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BT3.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BT3.Controllers
+{
+    public class StudentController : Controller
+    {
+        private static List<Student> _registeredStudents = new List<Student>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                _registeredStudents.Add(student);
+                return RedirectToAction("ShowKQ", student);
+            }
+            return View(student);
+        }
+
+        public IActionResult ShowKQ(Student student)
+        {
+            int countSameMajor = _registeredStudents.Count(s => s.ChuyenNganh == student.ChuyenNganh);
+
+            ViewBag.MSSV = student.MSSV;
+            ViewBag.HoTen = student.HoTen;
+            ViewBag.ChuyenNganh = student.ChuyenNganh;
+            ViewBag.SoLuongCungNganh = countSameMajor;
+
+            return View();
+        }
+    }
+}
---
+++ Buoi_3/Models/Student.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BT3.Models
+{
+    public class Student
+    {
+        [Required(ErrorMessage = "Vui lòng nhập Mã số sinh viên")]
+        [Display(Name = "Mã số sinh viên")]
+        public string? MSSV { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Họ tên")]
+        [Display(Name = "Họ tên")]
+        public string? HoTen { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng nhập Điểm trung bình")]
+        [Display(Name = "Điểm Trung bình")]
+        [Range(0, 10, ErrorMessage = "Điểm trung bình phải từ 0 đến 10")]
+        public double DiemTB { get; set; }
+
+        [Required(ErrorMessage = "Vui lòng chọn Chuyên ngành")]
+        [Display(Name = "Chuyên ngành")]
+        public string? ChuyenNganh { get; set; }
+    }
+}
